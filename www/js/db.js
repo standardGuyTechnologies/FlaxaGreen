6afa21c0bde37b0bdc1e4be4a8b3986cc1db8055
iDeveloper <1eyeddeveloper@gmail.com>
2023-12-22 05:39:36 -0800

    Edited some columns on the database to facilitate the table joins I pictured in my head when trying to render some information

--- www/js/db.js
+++ www/js/db.js
@@ -3,7 +3,7 @@ var db;
 function initDB(){
   return new Promise ((resolve, reject) => {
     if (process.env.ENVIRON === 'electron') {
-      db = openDatabase('flaxadb', '1.0', 'Accounting pplication database', 5 * 1024 * 1024);
+      db = openDatabase('flaxadb', '1.0', 'Accounting application database', 5 * 1024 * 1024);
     } else if (process.env.ENVIRON === 'cordova') {
       db = window.sqlitePlugin.openDatabase({
         name: 'flaxadb', location: 'default'
@@ -11,12 +11,12 @@ function initDB(){
     }
     db.transaction(function (tx) {
       /* date is converted to Utcms before storage */
-      tx.executeSql('CREATE TABLE IF NOT EXISTS  ACC (acc, bal)');
-      tx.executeSql('CREATE TABLE IF NOT EXISTS  QUICK (date INT NOT NULL, acc NOT NULL, categ NOT NULL, subcateg NOT NULL, item NOT NULL, amt DEFAULT 0, qty, location, info)');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS  ACCOUNTS (acc, bal)');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS  QUICK (date INT NOT NULL, acc NOT NULL, categ NOT NULL, subcateg NOT NULL, item NOT NULL, amt DEFAULT 0, qty, target, location, info)');
       tx.executeSql('CREATE TABLE IF NOT EXISTS  QUICKDIFF (date INT NOT NULL, acc NOT NULL, qdiff DEFAULT 0)');
       tx.executeSql('CREATE TABLE IF NOT EXISTS TRACK (id, categ, subcateg, state, party, assign)');
-      tx.executeSql('CREATE TABLE IF NOT EXISTS TRACKDIFF (id, date, acc, tdiff DEFAULT 0)');
-      tx.executeSql('CREATE TABLE IF NOT EXISTS TRACKPHASE (id, info, phase DEFAULT 1, amt DEFAULT 0, repaid DEFAULT 0, forfeit DEFAULT 0)');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS TRACKDIFF (date, acc, tdiff DEFAULT 0)');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS TRACKPHASE (id, date, acc, info, phase DEFAULT 1, val DEFAULT 0, type)'); // type is of amt, repaid, forfeit
     }, function(e) {reject(e)}, function(){resolve()})
   })
 }
@@ -33,7 +33,7 @@ function wipeDb(db, opts) {
   return cordova.plugins.sqlitePorter.wipeDb(db, opts)
 }
 function dBList() { 
-  return ["ACC","QUICK", "QUICKDIFF", "TRACK","TRACDIFF", "TRACKPHASE"]
+  return ["ACCOUNTS","QUICK", "QUICKDIFF", "TRACK","TRACDIFF", "TRACKPHASE"]
 }
 function getDB(){ return db }
 export {initDB, importDb, exportDb, wipeDb, dBList};

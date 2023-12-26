32427afc80204efcb421279e751d99e9a5249623
iDeveloper <1eyeddeveloper@gmail.com>
2023-12-26 02:18:38 -0800

    Added function to manage mock data on the database script

--- www/js/db.js
+++ www/js/db.js
@@ -12,11 +12,20 @@ function initDB(){
       reject ('Not a supported platform for flaxa database')
     };
     db.transaction(function (tx) {
+      tx.executeSql('drop table if exists CONFIG')
+      tx.executeSql('drop table if exists QUICK')
+      tx.executeSql('drop table if exists QUICKDIFF')
+      tx.executeSql('drop table if exists TRACK')
+      tx.executeSql('drop table if exists TRACKDIFF')
+      tx.executeSql('drop table if exists TRACKPHASE')
+      tx.executeSql('drop table if exists ACCOUNTS')
+    }, function (e) {reject(e)}, function () {});
+    db.transaction(function (tx) {
       /* date is converted to Utcms before storage */
-      tx.executeSql('CREATE TABLE IF NOT EXISTS  CONFIG (currency, currindex, delimeter)');
-      tx.executeSql('CREATE TABLE IF NOT EXISTS  ACCOUNTS (acc, bal)');
-      tx.executeSql('CREATE TABLE IF NOT EXISTS  QUICK (date INT NOT NULL, acc NOT NULL, categ NOT NULL, subcateg NOT NULL, item NOT NULL, amt DEFAULT 0, qty, target, location, info)');
-      tx.executeSql('CREATE TABLE IF NOT EXISTS  QUICKDIFF (date INT NOT NULL, acc NOT NULL, qdiff DEFAULT 0)');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS CONFIG (currency, currindex, delimeter)');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS ACCOUNTS (acc, bal)');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS QUICK (date INT NOT NULL, acc NOT NULL, categ NOT NULL, subcateg NOT NULL, item NOT NULL, amt DEFAULT 0, qty, target, location, info)');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS QUICKDIFF (date INT NOT NULL, acc NOT NULL, qdiff DEFAULT 0)');
       tx.executeSql('CREATE TABLE IF NOT EXISTS TRACK (id, categ, subcateg, state, party, intent)');
       tx.executeSql('CREATE TABLE IF NOT EXISTS TRACKDIFF (date, acc, tdiff DEFAULT 0)');
       tx.executeSql('CREATE TABLE IF NOT EXISTS TRACKPHASE (id, date, acc, info, val DEFAULT 0, type)'); // type is of amt, repaid, forfeit

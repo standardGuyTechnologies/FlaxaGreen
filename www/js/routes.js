063911d6404f8426b68ab216a6d01cff10d94588
iDeveloper <1eyeddeveloper@gmail.com>
2023-12-29 10:37:22 -0800

    Updated a part of routes to use the store state managing accounts.

--- www/js/routes.js
+++ www/js/routes.js
@@ -134,12 +134,9 @@ export default [
   {
     path: '/general-info/',
     async: function ({ app, resolve, reject }) {
-      const db = getDB(); let query1, query2; const activetrack = [];
+      const db = getDB(); let query2; const activetrack = [];
       let trackamt = {}, trackr = {}, trackf = {};
       db.transaction(function (tx) {
-        tx.executeSql('SELECT * FROM ACCOUNTS ORDER BY acc', [], function (tx, result) {
-          query1 = result.rows;
-        })
         tx.executeSql('SELECT acc, SUM(qdiff) AS qnet, SUM(tdiff) AS tnet FROM QUICKDIFF LEFT JOIN TRACKDIFF USING(date, acc) GROUP BY acc ORDER BY acc', [], function (tx, result) {
           query2 = result.rows;
         })
@@ -173,8 +170,9 @@ export default [
         })
       }, function (e) {reject(); console.log(e)}, function () {
         const arr = []; let i = 0, j = 0;
+        const query1 = app.store.getters.getaccobj.value;
         for(; i<query1.length; i++){
-          let x = query1.item(i); x.bal += query2.item(i).qnet + query2.item(i).tnet;
+          let x = query1[i]; x.bal += query2.item(i).qnet + query2.item(i).tnet;
           arr.push(x); j += x.bal;
         }
         resolve(
